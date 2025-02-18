dce00e95786c850f928ae3acf36e96cbfd800274
HopeCollector <cmw0249@gmail.com>
2025-02-18 14:15:25 +0800

    doc(tskpub.cc): add comments

--- source/tskpub.cc
+++ source/tskpub.cc
@@ -15,11 +15,17 @@ namespace {
 
 namespace tskpub {
   TSKPub::TSKPub(const std::string &config_file) {
+    // Load config file
     GlobalParams::get_instance().load_params(config_file);
+    // Init logger
     Log::init();
+
+    // Get all sensor names from config file
     auto sensors = GlobalParams::get_instance()
                        .yml["sensors"]
                        .get_value<std::vector<std::string>>();
+
+    // Create reader for each sensor
     for (auto &sensor_name : sensors) {
       auto &params = tskpub::GlobalParams::get_instance().yml[sensor_name];
       const auto &type = params["type"].get_value_ref<const std::string &>();
@@ -28,17 +34,22 @@ namespace tskpub {
   }
 
   TSKPub::~TSKPub() {
+    // Destroy global params
     GlobalParams::get_instance().destroy();
+    // Destroy logger
     Log::destory();
   }
 
   MsgConstPtr TSKPub::read(const std::string &sensor_name) const {
     auto it = sensor_reader_map.find(sensor_name);
     if (it == sensor_reader_map.end()) {
+      // if sensor_name is not found, return nullptr
       Log::critical("No reader for sensor: " + sensor_name);
       return nullptr;
     }
     auto msg = it->second->read();
+
+    // Update total read bytes
     GlobalParams::get_instance().total_read_bytes += msg ? msg->size() : 0;
     return msg;
   }
