3cff79c09f3110b3d129a778887e1f0bb913f415
HopeCollector <cmw0249@gmail.com>
2025-02-18 14:17:12 +0800

    doc(log): add comments for logging initialization and destruction

--- source/common.cc
+++ source/common.cc
@@ -17,15 +17,20 @@ namespace tskpub {
   std::shared_ptr<spdlog::logger> Log::logger_ = nullptr;
 
   void Log::init() {
+    // get log params
     auto& params = GlobalParams::get_instance().yml["log"];
+
+    // create logger according to where to log
     const auto& dst = params["filename"].get_value_ref<const std::string&>();
-    if (dst == "stdout") {
+    if (dst == "stdout") {  // log to stdout
       logger_ = spdlog::stdout_color_mt("console");
-    } else if (dst == "stderr") {
+    } else if (dst == "stderr") {  // log to stderr
       logger_ = spdlog::stderr_color_mt("console");
-    } else {
+    } else {  // log to file
       logger_ = spdlog::basic_logger_mt("file_logger", dst);
     }
+
+    // set log level and pattern
     logger_->set_level(static_cast<spdlog::level::level_enum>(
         params["level"].get_value<int>()));
     logger_->set_pattern(params["pattern"].get_value_ref<const std::string&>());
@@ -33,6 +38,7 @@ namespace tskpub {
 
   void Log::destory() {
     if (!logger_) return;
+    // unreferece logger
     spdlog::drop(logger_->name());
     logger_ = nullptr;
   }
